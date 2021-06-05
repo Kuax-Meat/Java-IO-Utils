16ca22cd9f665b3a8e610c4c70e2cb32a67d0fef
Jaehyeong Sim <jh.sim@kuax.org>
2021-06-05 23:18:13 +0900

    Json Parser

---
+++ src/org/kuax/lib/json/JsonUtils.java
@@ -0,0 +1,24 @@
+package org.kuax.lib.json;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.google.gson.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.util.*;
+
+public class JsonUtils {
+
+    public static String makeJsonStrFromMap(Map inputMap) {
+        return new Gson().toJson(inputMap, HashMap.class);
+    }
+
+    public static String makeJsonStrFromList(List inputList) {
+        return new Gson().toJson(inputList, ArrayList.class);
+    }
+
+    public static Map<String, Object> makeMapFromJson(String inputJson) throws JsonProcessingException {
+        return new ObjectMapper().readValue(inputJson, Map.class);
+    }
+}
